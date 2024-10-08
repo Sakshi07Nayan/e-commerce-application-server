d6e5f9da083880b019096ab940175c9dcd6dd6b1
Sakshi Nayan <sakshi12princi@gmail.com>
2024-10-08 11:49:12 +0530

    cors valid change

--- server.js
+++ server.js
@@ -31,18 +31,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-const allowedOrigins = ['http://localhost:3000'];
+// const allowedOrigins = ['http://localhost:3000'];
 
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true, 
+// }));
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true, 
+  origin: 'https://e-commerce-apllication-client.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // Enable this if you're sending cookies or using sessions
 }));
 
 app.use((req, res, next) => {
