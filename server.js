544db5d9974ba7a93fdf25cb0d6fbe2d4f8e397f
Sakshi Nayan <sakshi12princi@gmail.com>
2024-10-08 11:39:27 +0530

    get serve

--- server.js
+++ server.js
@@ -184,6 +184,9 @@ app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/product', customerReviews);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js API!');
+});
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
